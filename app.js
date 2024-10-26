1c3c2024a6d3031c382a950de42cbf1bfe84abec
Quirianyailet Perez <qperez1@usf.edu>
2024-10-26 18:33:06 -0400

    Add canvas setup and event handling logic.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 // Task 1: Set Up the HTML Structure (Refer to html file)
 
-// Task 2: 
+// Task 2: Configure the JavaScript for Drawing Context
+// Get the canvas element
+const canvas = document.getElementById('myCanvas');
+
+// Get the 2D drawing context
+const ctx = canvas.getContext('2d');
+
+// Add Mouse Movement Events
+document.querySelector("html").addEventListener("mousemove", event => {
+    console.log(`X: ${event.clientX} Y: ${event.clientY}`);
+})
