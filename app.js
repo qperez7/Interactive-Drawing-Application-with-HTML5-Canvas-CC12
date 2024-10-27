354e43d3b589f948cc22f51a3e2e80f945950407
Quirianyailet Perez <qperez1@usf.edu>
2024-10-26 21:26:19 -0400

    Implement shape drawing logic for lines, rectangles, and circles

--- app.js
+++ app.js
@@ -30,12 +30,26 @@ canvas.addEventListener("mousemove", (event) => {
     y = event.clientY;
     Drawing = true;
 });
+// Task 3: Implement Shape Drawing Logic
 function drawShape(ctx, x1, y1, x2, y2) {
+    let toolOption = toolOption.querySelector("radio") // Need to properly reference radio tool <---
+    if(toolOption === Rectangle) {
+        ctx.strokeStyle = '#e74c3c';
+        ctx.lineWidth = 5;
+        ctx.strokeRect(250, 50, 150, 100);
+}
+else if (toolOption === Circles) {
     ctx.beginPath();
-    //ctx.colorSelector();
+    ctx.arc(150, 300, 50, 0, 2 * Math.PI);  // Draw a full circle
+    ctx.fillStyle = '#f1c40f';
+    ctx.fill();  // Fill the circle
+}
+else {
+    ctx.beginPath();
+    //ctx.colorSelector(); // Need to properly reference color selector <---
     ctx.lineWidth = 1;
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
     ctx.closePath();
-}
+}}
